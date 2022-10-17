b36b4012a108eaeed4dde800ff3e7172e5e3e647
sierra182 <sierra-echo@msn.com>
2022-10-17 16:36:51 +0400

    add management scale photo with landcape

--- Kioxk/Client/wwwroot/JavaScript.js
+++ Kioxk/Client/wwwroot/JavaScript.js
@@ -20,6 +20,7 @@ function resizeIframe(obj) {
                 f.style.setProperty("transition", "none");
                 f.style.height = 0; f.style.height = f.contentWindow.document.documentElement.scrollHeight + 'px';
 
+                aftergardPhoto();
             }, 1000);
         });
 
@@ -34,6 +35,67 @@ function resizeIframe(obj) {
     }
 }
 
+function aftergardPhoto() {
+    if (togScal) {
+
+        phs = document.getElementById("photos");
+        allimg = phs.getElementsByTagName("img");
+
+        const land = window.matchMedia("(orientation:landscape)");
+        const port = window.matchMedia("(orientation:portrait)");
+
+                                                             console.log("land et port: " + land.matches + " " + port.matches);
+        if (land.matches) {
+            console.log("after land true");
+
+            phs.style.setProperty("width", "auto");
+            for (var oneimg of allimg) {
+                oneimg.style.width = "auto";
+                oneimg.style.height = "95vh";
+
+            }
+
+            var imgcs;
+            for (var oneimg of allimg) {
+
+                imgcs = getComputedStyle(oneimg);
+            }
+            const phss = getComputedStyle(phs);
+
+                                                                     console.log("phss: " + phss.width + "imgcs: " + imgcs.width);
+            var copimg = Math.round(parseFloat(imgcs.width));
+            var copph = Math.round(parseFloat(phss.width));
+                                                             console.log("ROUNDED phss: " + copph + "imgcs: " + copimg);
+            if (copph < copimg) {
+                                                                     console.log("cont est plus petit que img");
+
+                                                                           console.log("forced portrait: " + copph + " " + copimg);
+                phs.style.setProperty("width", "95vw");
+                for (var oneimg of allimg) {
+                    oneimg.style.width = "100%";
+                    oneimg.style.height = "auto";
+
+                }
+
+            }
+            else {
+                console.log("cont n'est pas plus petit que img");
+            }
+
+        }
+
+        else if (port.matches) {
+                                                         console.log("after port true");
+            phs.style.setProperty("width", "95vw");
+
+            for (var oneimg of allimg) {
+                oneimg.style.width = "100%";
+                oneimg.style.height = "auto";                
+            }
+        }                
+    }
+}
+
 var togScal = false;
 var phcont
 var maxs;
@@ -43,6 +105,7 @@ var contifra;
 var ifra;
 var myvarcp;
 var contmlt;
+var allimg;
 var contal;
 var myvarcontal;
 var myvartrans;
@@ -52,9 +115,11 @@ var car2;
 function scaler(docu, carre, carre2, xx, yy, x, y) {
 
     phs = document.getElementById("photos");
+    phss = getComputedStyle(phs);
     phcont = phs.firstChild;
     contifra = document.getElementsByClassName("contIfraJs");
     contmlt = document.getElementsByClassName("contPhMultJs");
+    allimg = phs.getElementsByTagName("img");
     contal = document.getElementById("contPhAloneJs");
     car = document.getElementById(carre);
     car2 = document.getElementById(carre2);
@@ -63,26 +128,42 @@ function scaler(docu, carre, carre2, xx, yy, x, y) {
 
         togScal = true;
 
-        function res() {
+        myvarcontal = getComputedStyle(contal).getPropertyValue("--ph_scale-height");
+        myvarcp = getComputedStyle(phs).getPropertyValue("--cp_mult-h");
+        myvarscale = getComputedStyle(phs).getPropertyValue("--mult_ph-scale");
+        myvartrans = getComputedStyle(phs).getPropertyValue("--trans-height");
+        maxs = getComputedStyle(phs).getPropertyValue("--max_size");
 
-            if (togScal) {
+        phs.style.position = "absolute";
 
-                myvarcontal = getComputedStyle(contal).getPropertyValue("--ph_scale-height");
-                myvarcp = getComputedStyle(phs).getPropertyValue("--cp_mult-h");
-                myvarscale = getComputedStyle(phs).getPropertyValue("--mult_ph-scale");
-                myvartrans = getComputedStyle(phs).getPropertyValue("--trans-height");
-                maxs = getComputedStyle(phs).getPropertyValue("--max_size");
-                phs.style.position = "absolute";
-                phs.style.setProperty("--mult_ph-scale", myvarscale * 3.5);
-                phs.style.setProperty("--max_size", "0px");
-                phcont.style.setProperty("height", "fit-content");
+        const land = window.matchMedia("(orientation:landscape)");
+        const port = window.matchMedia("(orientation:portrait)");
+        if (land.matches) {
+            console.log("land true");
+            phs.style.setProperty("width", "auto");
+            for (var oneimg of allimg) {
+                oneimg.style.width = "auto";
+                oneimg.style.height = "95vh";
+                oneimg.style.maxHeight = "none";
+            }
 
-                for (var cif of contifra) {
-                    cif.firstChild.style.position = "relative";
-                }               
+        }
+        else if (port.matches) {
+            console.log("port true");
+            phs.style.setProperty("width", "95vw");
+            for (var oneimg of allimg) {
+                oneimg.style.width = "100%";
+                oneimg.style.height = "auto";
+                oneimg.style.maxHeight = "none";
             }
         }
-        res();
+
+        phcont.style.setProperty("height", "fit-content");
+
+        for (var cif of contifra) {
+            cif.firstChild.style.position = "relative";
+        }
+
 
         phs.style.borderRadius = ".1%";
         phs.style.zIndex = "3";
@@ -108,14 +189,23 @@ function scaler(docu, carre, carre2, xx, yy, x, y) {
         car2.style.borderColor = "red";
         car2.style.opacity = ".5";
         car2.style.borderRadius = "0 50% 0 0";
-       
+
+
+        phcont.style.height = "var(--cp_ph-y)";
+        // phs.style.setProperty("--mult_ph-scale", myvarscale);
+        //  phs.style.setProperty("--max_size", maxs);
+
+        for (var oneimg of allimg) {
+            oneimg.style.width = "100%";
+            oneimg.style.height = "auto";
+            oneimg.style.maxHeight = "calc(max(var(--photo-width),var(--max_size)) / 2 * .6)";
+        }
+        phs.style.setProperty("width", "calc(max(var(--photo-width),var(--max_size)) / 2 * var(--mult_ph-scale))");
+        //phs.style.setProperty("height", "fit-content");
+        //  phss.style.width = "calc(max(var(--photo - width),var(--max_size)) / 2 * var(--mult_ph-scale))";
         for (var cif of contifra) {
             cif.firstChild.style.position = "absolute";
         }
-        phcont.style.height = "var(--cp_ph-y)";
-        phs.style.setProperty("--mult_ph-scale", myvarscale);
-        phs.style.setProperty("--max_size", maxs);
-      
         window.setTimeout(zin, 1000, true);
         function zin() {
             phs.style.zIndex = "0";
@@ -124,18 +214,18 @@ function scaler(docu, carre, carre2, xx, yy, x, y) {
 }
 
 function scroll(el, sens) {
-    
+
     var elem = document.getElementById(el);
 
-    if (sens === "bas") {       
+    if (sens === "bas") {
         setTimeout(() => {
             var coo = elem.scrollHeight;
-            elem.scrollTo(0, coo);            
+            elem.scrollTo(0, coo);
         }, 2000)
 
-    } else if (sens === "haut") {       
+    } else if (sens === "haut") {
         var coo = elem.scrollHeight;
-        elem.scrollTo(0, -coo);       
+        elem.scrollTo(0, -coo);
     };
 }
 
