cdc35476889b4746aa4275e7ebb9a139a795259e
sierra182 <sierra-echo@msn.com>
2022-10-23 10:50:25 +0400

    add navigator filter

---
+++ Kioxk/Client/wwwroot/Start.js
@@ -0,0 +1,18 @@
+﻿function main() {    
+    var isChrome = !!window.chrome;   
+        
+        if (isChrome && getChromeVersion() < 100) {
+
+            alert("Il se peut que votre navigateur ne soit pas à jour. Si vous rencontrez des problemes d'affichage ou d'utilisation, veuillez le mettre à jour ou utilisez Chrome à jour..!");
+           // window.location.reload();
+        }
+        if (!isChrome) {
+            alert("Cette Application est optimisée pour les navigateurs basés sur Chromium: ( Chrome, Edge, Opera...). Si vous rencontrez des problemes d'affichage ou d'utilisation, téléchargez un navigateur basé sur Chromium.");
+            //window.location.reload();
+        }
+    }
+
+function getChromeVersion() {
+    var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+    return raw ? parseInt(raw[2], 10) : 0;
+}
