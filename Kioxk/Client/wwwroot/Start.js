f2fb633a12ba4a1a170dc332100e97088ef814e4
sierra182 <sierra-echo@msn.com>
2022-10-23 11:43:06 +0400

    fix main js start

--- Kioxk/Client/wwwroot/Start.js
+++ Kioxk/Client/wwwroot/Start.js
@@ -1,18 +1,32 @@
-﻿function main() {    
-    var isChrome = !!window.chrome;   
-        
-        if (isChrome && getChromeVersion() < 100) {
+﻿(function () {  
+    var isChrome = !!window.chrome;
 
-            alert("Il se peut que votre navigateur ne soit pas à jour. Si vous rencontrez des problemes d'affichage ou d'utilisation, veuillez le mettre à jour ou utilisez Chrome à jour..!");
-           // window.location.reload();
-        }
-        if (!isChrome) {
-            alert("Cette Application est optimisée pour les navigateurs basés sur Chromium: ( Chrome, Edge, Opera...). Si vous rencontrez des problemes d'affichage ou d'utilisation, téléchargez un navigateur basé sur Chromium.");
-            //window.location.reload();
-        }
+    if (isChrome && getChromeVersion() < 100) {
+
+        alert("Il se peut que votre navigateur ne soit pas à jour. Si vous rencontrez des problemes d'affichage ou d'utilisation, veuillez le mettre à jour ou utilisez Chrome à jour..!");
+        // window.location.reload();
     }
+    if (!isChrome) {
+        alert("Cette Application est optimisée pour les navigateurs basés sur Chromium: ( Chrome, Edge, Opera...). Si vous rencontrez des problemes d'affichage ou d'utilisation, téléchargez un navigateur basé sur Chromium.");
+        //window.location.reload();
+    }
+}
+)();
 
 function getChromeVersion() {
     var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
     return raw ? parseInt(raw[2], 10) : 0;
+}
+
+function cookies() { //on work...
+
+    if (localStorage.getItem("yetconnect")) {
+
+        alert('Une mise à jour a été téléchargée.'); window.location.reload();
+    }
+    else {
+
+        localStorage.setItem("yetconnect", true);
+
+    }
 }
