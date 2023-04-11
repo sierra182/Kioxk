3f59660451c715ea43bc032a6c949872d5f0f0dc
sierra182 <sierra-echo@msn.com>
2023-04-11 12:58:59 +0400

    fix bug clickallweek & pointer elements

--- Kioxk/Client/Shared/MyEvents.cs
+++ Kioxk/Client/Shared/MyEvents.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Kioxk.Shared.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace Kioxk.Client.Shared;
     public class MyEvents
     {
+    public Livret? Livret { get; set; }
     public delegate void SubscriptionHandler();
-    public event SubscriptionHandler ?AddSubscriptionSync;
+    public event SubscriptionHandler? AddSubscriptionSync;
     public event Action? RemoveSubscriptionSync;
     public Action? MyPointer;
+    public Action? HelpUpdate;
 
     public void RiseAddSubscriptionSyncEvent()=> AddSubscriptionSync?.Invoke();
     public void RiseRemoveSubscriptionSyncEvent() => RemoveSubscriptionSync?.Invoke();
     public void RisePointerEvent()=> MyPointer?.Invoke();
+    public void RiseHelpUpdateEvent() => HelpUpdate?.Invoke();
 }
 
